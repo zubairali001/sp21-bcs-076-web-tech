bd2af00b859af506c799d0fb3ce7bae7105bab82
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-04 15:25:00 +0500

    add mongoose

--- MyApp/server.js
+++ MyApp/server.js
@@ -1,9 +1,23 @@
 // Server Start.
-
 const express = require("express");
 const app = express();
 const port = 5100;
 
+// Require Mongoose
+const mongoose = require("mongoose");
+const url = "mongodb+srv://zubairworkspace:katgyw-0hijxa-rIkraw@zubair.bvy4m1e.mongodb.net/portfolioDatabase?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+}).then(() => {
+  console.log("Succcessfully connected to Mongoose Database, Greate Zubair!!");
+}).catch((e) => {
+  console.log("Error is:" +e);
+})
+
+// Connection String.
+// mongodb+srv://zubairworkspace:katgyw-0hijxa-rIkraw@zubair.bvy4m1e.mongodb.net/portfolioDatabase?retryWrites=true&w=majority
 // App Start through port.
 
 app.listen(port, () => {
@@ -11,11 +25,8 @@ app.listen(port, () => {
 })
 
 // Ejs use to embed java script in html file. When move on browser, ejs will seperate the html and java script file.
-
 // Set the ejs
 app.set("view engine", "ejs");
-
-
 
 // To declare the public folder as static folder.
 app.use(express.static("public"));
@@ -25,9 +36,19 @@ const expressLayout = require("express-ejs-layouts");
 app.set("layout", "./layouts/main_layouts");
 app.use(expressLayout);
 
-// get the ejs.
+// Getting the ejs.
 app.get("/", (req, res)=> {
   res.render('landing_page');
+});
+
+app.get("/login", (req, res) => {
+  const path = "auth/login";
+  res.render(path);
+});
+
+app.get("/register", (req, res) => {
+  const path = "auth/register";
+  res.render(path);
 });
 
 // run following command to install express
