f6131fcbe650fd33015732b6de26676c5abcfe6c
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-07 13:03:50 +0500

    Update login_user.js

--- MyApp/functions/login_user.js
+++ MyApp/functions/login_user.js
@@ -1,14 +1,14 @@
 const signUpModel = require("../models/register_model");
 
-const isUserExists = findUserByEmailAndPassword async(res, email, password) {
-  const user =  await signUpModel.findOne({ email: email, password: password });
+async function findUserByEmailAndPassword(email, password) {
+  const user = await signUpModel.findOne({ email: email, password: password });
 
-  if(user){
-    req.session.user = { id: user._id, username: user.username };
+  if (user) {
+    // req.session.user = { id: user._id, username: user.username };
     return true;
-  }else{
-    return false;;
+  } else {
+    return false;
   }
 }
 
-module.exports.isUserExists = isUserExists;
+module.exports.findUserByEmailAndPassword = findUserByEmailAndPassword;
