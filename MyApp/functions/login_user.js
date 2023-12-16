823230f559de1f80f645b6e7a2eafebfee03fc89
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-16 16:23:21 +0500

    Update login_user.js

--- MyApp/functions/login_user.js
+++ MyApp/functions/login_user.js
@@ -4,7 +4,7 @@ async function findUserByEmailAndPassword(email, password) {
   const user = await signUpModel.findOne({ email: email, password: password });
 
   if (user) {
-    // req.session.user = { id: user._id, username: user.username };
+   // req.session.user = { id: user._id, username: user.username };
     return true;
   } else {
     return false;
