45029a9771f6d90ac6ad8af8bc140f045ac1b82a
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-16 21:04:26 +0500

    Update hirePageRoute.js

--- MyApp/routes/site/hirePageRoute.js
+++ MyApp/routes/site/hirePageRoute.js
@@ -1,12 +1,26 @@
 const express = require("express");
 let router = express.Router();
 
+const { placeUserOrder, fetchUserOrders} = require("../../operations/placeOrderOperation");
 const authenticatedUser = require("../../middlewares/auth_authentication");
 // Hire page Route ==========================:
 
-router.get("/hireme", authenticatedUser,(req, res) => {
+router.get("/hireme", authenticatedUser, async(req, res) => {
   const path = "hire/hirePage";
-  res.render(path, { showHeader: true });
+  let orders = await fetchUserOrders();
+  res.render(path, {orders});
 });
+
+router.post("/hireme", async (req, res) => {
+  try {
+    const userEmail = req.session.userEmail;
+    console.log("User Email is: "+userEmail);
+    await placeUserOrder(res, req.body.name, userEmail, req.body.requiremnts, req.body.price, req.body.message);
+
+    return;
+  } catch (error) {
+    console.error("Error occur while placing user order: ", error);
+  }
+})
 
 module.exports = router;
