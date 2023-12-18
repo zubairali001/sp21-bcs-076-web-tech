4ec079ebc1c04f078a0ea2d889330ab3f57035ea
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-18 12:43:33 +0500

    Update hirePageRoute.js

--- MyApp/routes/site/hirePageRoute.js
+++ MyApp/routes/site/hirePageRoute.js
@@ -9,7 +9,7 @@ const authenticatedUser = require("../../middlewares/auth_authentication");
 
 router.get("/hireme", authenticatedUser, async(req, res) => {
   const path = "hire/hirePage";
-  let orders = await fetchUserOrders();
+  let orders = await fetchUserOrders(req.session.userEmail);
   res.render(path, {orders});
 });
 
@@ -44,5 +44,59 @@ router.post("/hireme/delete", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
+router.get("/editOrder/:id", async (req, res) => {
+ console.log("Request body is: " + req.params.id);
+
+  try {
+    const updatedOrder = await orderModel.findOne(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+
+    console.log(updatedOrder.name + " " + updatedOrder.requirement + " " + updatedOrder.message);
+
+    if (!updatedOrder) {
+      console.log("Error: Order not found");
+      res.status(404).send("Order not found");
+      return;
+    }
+
+    res.render('hire/editOrder', { updateOrder: updatedOrder });
+  } catch (error) {
+    console.log("Error while updating:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/editOrder/:id", async (req, res) => {
+ console.log("Request body is: " + req.params.id);
+
+  try {
+
+    let model = await orderModel.findById(req.params.id);
+
+    const userEmail = req.session.userEmail;
+
+    if(model){
+      model.name = req.body.name;
+      model.email = userEmail;
+      model.requirement = req.body.requirement;
+      model.message = req.body.message;
+
+      await model.save();
+      res.redirect('/hireme');
+    }else{
+      console.log("Model not found..");
+    }
+
+
+  } catch (error) {
+    console.log("Error while updating:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 module.exports = router;
