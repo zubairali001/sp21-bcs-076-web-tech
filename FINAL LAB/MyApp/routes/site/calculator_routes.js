989503f3c2fee51784c7c792dbf07f77e6ce74c6
zubairali.gitlab <zubair.workspace@gmail.com>
2023-12-22 16:30:45 +0500

    Update calculator_routes.js

--- FINAL LAB/MyApp/routes/site/calculator_routes.js
+++ FINAL LAB/MyApp/routes/site/calculator_routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 let router = express.Router();
 
+const {calculate} = require("../../functions/calculate");
+
 router.get("/calculator",(req,res)=>{
     req.session.resultsArray = req.session.resultsArray || [];
     const operations = req.session.resultsArray;
@@ -12,53 +14,11 @@ router.post("/calculator",(req,res)=>{
     const operand1 = req.body.num1
     const operand2 = req.body.num2
 
-    const num1 = parseFloat(operand1);
-    const num2 = parseFloat(operand2);
-
-    if (isNaN(num1) || isNaN(num2)) {
-        return res.status(400).json({ error: 'Invalid operands' });
+    try{
+        calculate(req, res, operand1, operand2, req.body.operand);
+    }catch(e){
+        console.log(e);
     }
-    let result;
-
-     let operand = req.body.operand;
-    switch(operand){
-        case '+':
-            result = num1 + num2
-            break
-        case '-':
-            result = num1 - num2
-            break
-        case '/':
-            result = num1 / num2
-            break
-        case '*':
-            result = num1 * num2
-            break
-
-    }
-    if (isNaN(result) || !isFinite(result)) {
-        return res.status(400).json({ error: 'Invalid result' });
-    }
-    console.log(result)
-    result = parseFloat(result)
-    const resultObject = {
-        num1:num1,
-        num2:num2,
-        operand:operand,
-        result: result
-
-    }
-
-    req.session.resultsArray.push( {
-        num1:num1,
-        num2:num2,
-        operand:operand,
-        result: result
-    })
-    console.log(req.session.resultsArray)
-    const operations = req.session.resultsArray
-    console.log(operations)
-    res.redirect("/calculator")
 })
 
 module.exports = router
